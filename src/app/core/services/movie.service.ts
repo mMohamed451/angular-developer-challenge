232f4a3b27ae6698adb473199663e9147dd28a53
unknown <m.mohamed451@hotmail.com>
2022-02-16 21:47:19 +0200

    [refactoring] css classes

--- src/app/core/services/movie.service.ts
+++ src/app/core/services/movie.service.ts
@@ -3,31 +3,31 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MoviesService {
-  constructor(private httpClient: HttpClient) {
-  }
+  constructor(private httpClient: HttpClient) {}
 
   getAllMovies() {
-    return this.httpClient.get(`${environment.url}/genre/28/movies?api_key=${environment.apiKey}&language=en-US&page=1`);
-
+    return this.httpClient.get(
+      `${environment.url}/genre/28/movies?api_key=${environment.apiKey}&language=en-US&page=1`
+    );
   }
   searchForSpecificMovie() {
-    // allowed values
-    // popularity.asc, popularity.desc, release_date.asc, release_date.desc, revenue.asc,
-    // revenue.desc,primary_release_date.asc, primary_release_date.desc, original_title.asc,
-    // original_title.desc, vote_average.asc, vote_average.desc, vote_count.asc, vote_count.desc
-    return this.httpClient.get(`${environment.url}/discover/movie?api_key=${environment.apiKey}&language=en-US&sortby=original_title.desc&page=1`);
+    return this.httpClient.get(
+      `${environment.url}/discover/movie?api_key=${environment.apiKey}&language=en-US&sortby=original_title.desc&page=1`
+    );
   }
 
   getMovieDetails(movieId: any) {
-    return this.httpClient.get(`${environment.url}/movie/${movieId}?api_key=${environment.apiKey}`);
+    return this.httpClient.get(
+      `${environment.url}/movie/${movieId}?api_key=${environment.apiKey}`
+    );
   }
 
   getMovieReviews(movieId: any) {
-    return this.httpClient.get(`${environment.url}/movie/${movieId}/reviews?api_key=${environment.apiKey}`);
+    return this.httpClient.get(
+      `${environment.url}/movie/${movieId}/reviews?api_key=${environment.apiKey}`
+    );
   }
-
-
 }
